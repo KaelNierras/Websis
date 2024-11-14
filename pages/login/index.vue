1aefeefe4821589e774514112278bebf159cdaa8
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2024-11-14 14:28:51 +0800

    Simple Structure

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,11 +1,10 @@
-<template>
-    <style>
-        layout: {
-            width: 290.213px;
-            height: 65.066px;
-            flex-shrink: 0;
+<style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, Helvetica, sans-serif;
         }
-        typography: {
+        typography {
             color: #030303;
             text-align: center;
             font-family: Poppins;
@@ -16,8 +15,30 @@
             letter-spacing: 1.02px;
             text-transform: uppercase;
         }
-    </style>
-   <div style="width: 100vw m !important; height: 100vh !important;">
+</style>
+<template>
+    
+   <div style="width: 100vw !important; height: 100vh !important; display: flex; flex: 1;">
+        <div style="">
+            <h1 style="font-family: Arial, Helvetica, sans-serif;">Welcome Back</h1>
+            <p>Welcome back! Please enter your details.</p>
+            <form action="">
+                <label for="">Email</label>
+                <input type="text" name="" id="" placeholder="Enter your email">
+                <label for="">Password</label>
+                <input type="text" name="" id="" placeholder="Enter your password">
+                <div>
+                    <input type="checkbox">
+                    <span>Remember me</span>
+                    <span>Forgot password</span>
+                </div>
+                <button>Sign In</button>
+                <button>Sign In with Google</button>
+            </form>
+            <p>Don't have an account? <a href="">Sign up for free!</a></p>
+        </div>
+        <div>
 
+        </div>
    </div>
 </template>
