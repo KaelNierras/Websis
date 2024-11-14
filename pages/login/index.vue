667c55ae0c3571dc874aadccbc370a617ef990ad
John Rhuel Laurente <22-1-00109@vsu.edu.ph>
2024-11-14 14:17:34 +0800

    LogIn Demo

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,3 +1,23 @@
 <template>
-    This is the login page hehe
+    <style>
+        layout: {
+            width: 290.213px;
+            height: 65.066px;
+            flex-shrink: 0;
+        }
+        typography: {
+            color: #030303;
+            text-align: center;
+            font-family: Poppins;
+            font-size: 34px;
+            font-style: normal;
+            font-weight: 500;
+            line-height: normal;
+            letter-spacing: 1.02px;
+            text-transform: uppercase;
+        }
+    </style>
+   <div style="width: 100vw m !important; height: 100vh !important;">
+
+   </div>
 </template>
